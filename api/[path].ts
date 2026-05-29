b5e7ba131f427ca3e3dda357d219630d74c91d8a
mansa-kev <1kevinsjames@gmail.com>
2026-05-29 17:32:29 +0300

    Resolve Vercel API path conflicts

--- api/[path].ts
+++
@@ -1,8 +0,0 @@
-import app from "../server";
-
-export default function handler(req: any, res: any) {
-  if (typeof req.url === "string" && !req.url.startsWith("/api")) {
-    req.url = `/api${req.url.startsWith("/") ? req.url : `/${req.url}`}`;
-  }
-  return app(req, res);
-}
